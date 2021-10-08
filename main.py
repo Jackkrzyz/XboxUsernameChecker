8c92e99453d4b45d83699d56925a385b0118f91e
Jack Krzyz <jackkrzyz@gmail.com>
2021-10-08 13:36:21 -0500

    a

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def _3_letter_check_mixed():
         user = str(random.choice(string.ascii_letters)).lower() + str(shtuf[random.randrange(35)]).lower() + str(shtuf[random.randrange(35)]).lower()
         
         with open('checked.txt', 'a+') as c:
-            if user not in c.read():
+            if user not in c.readlines():
                 c.write(user + '\n')
                 proxie_list = []
                 with open("https.txt", "r") as f:                   
@@ -19,7 +19,7 @@ def _3_letter_check_mixed():
                 try:
                     r = requests.get(f'https://xboxgamertag.com/search/{user}', headers=headers, proxies=proxies, timeout=5)
                 except:
-                    print('REQUEST FAILED')
+                    print(f'REQUEST FAILED : {user}')
                 if r.status_code == 404:
                     with open('usernames.txt', 'a+') as t:
                         print(f'{user} : AVAILABLE')
@@ -32,7 +32,7 @@ def _3_letter_check_letter_only():
         user = random.choice(string.ascii_letters).lower() + random.choice(string.ascii_letters).lower() + random.choice(string.ascii_letters).lower()
         
         with open('checked.txt', 'a+') as c:
-            if user not in c.read():
+            if user not in c.readlines():
                     c.write(user + '\n')
                     proxie_list = []
                     with open("https.txt", "r") as f:                   
@@ -44,7 +44,7 @@ def _3_letter_check_letter_only():
                     try:
                         r = requests.get(f'https://xboxgamertag.com/search/{user}', headers=headers, proxies=proxies, timeout=5)
                     except:
-                        print('REQUEST FAILED')
+                        print(f'REQUEST FAILED : {user}')
                     if r.status_code == 404:
                         with open('usernames.txt', 'a+') as t:
                             print(f'{user} : AVAILABLE')
